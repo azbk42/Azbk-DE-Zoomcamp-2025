114caa5fc385a89659fee521309e1fa49a3413d1
azbk42 <mauduit.pro@outlook.com>
2025-01-29 13:28:47 +0100

    update scrit pipeline.py

--- ingestion.py
+++ ingestion.py
@@ -4,35 +4,61 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from time import time
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description= 'Ingest CSV data to Postgres')
+
+parser.add_argument('user', help='user name for postgres')
+parser.add_argument('password', help='password for postgres')
+parser.add_argument('host', help='host for postgres')
+parser.add_argument('port', help='port for postgres')
+parser.add_argument('db', help='database name for postgres')
+parser.add_argument('table-name', help='name of the table where we will write the results')
+parser.add_argument('url', help='url of the csv file')
 
 
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+def main(params):
 
-df_iter = pd.read_csv('yellow_tripdata_2021-01.csv', iterator=True, chunksize=100000)
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
 
-df = next(df_iter)
+    csv_name = 'output.csv'
 
-df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    os.system(f"wget {url} -O {csv_name}")
+    # download the csv
 
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+    engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{db}')
 
-get_ipython().run_line_magic('time', "df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')")
-
-while True: 
-    t_start = time()
+    df_iter = pd.read_csv(csv_name , iterator=True, chunksize=100000)
 
     df = next(df_iter)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-    
-    df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
 
-    t_end = time()
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-    print('inserted another chunk, took %.3f second' % (t_end - t_start))
+    while True: 
+        t_start = time()
+
+        df = next(df_iter)
+
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+
+        t_end = time()
+
+        print('inserted another chunk, took %.3f second' % (t_end - t_start))
 
 
-
+if __name__ == '__main__':
+    main()
 
